998edb9b7a5f2e20270fe58a2da2c550d7f1b6a0
AngelAngelopoulos <angelsuminoe01@gmail.com>
2018-11-11 11:10:10 -0600

    Update funciones

--- Juego v1.1/main.c
+++ Juego v1.1/main.c
@@ -1,4 +1,4 @@
-/**************************************************************************************************
+/******************************************************************************************
 *
 *
 *
@@ -33,6 +33,8 @@
 #include <math.h>
 #include <graphics.h>
 
+#define SIZE_STRINGS 30
+
 /*Estructura de tipo Sprite que es el archivo binario que lo representa*/
 typedef struct
 {
@@ -48,6 +50,7 @@ typedef struct nodeSprite
 {
     int sCorx;  //coordenada en x del sprite dentro de la malla
     int sCory;  //coordenada en y del sprite dentro de la malla
+    char nameSprite[SIZE_STRINGS];
     bool sVisible; /*en otro caso //Visiblidad dentro de la malla
     int sVisible;   */
     struct nodeSprite *nextSprite;  //Liga al siguiente sprite
@@ -60,14 +63,17 @@ typedef struct nodeLevel
 {
     struct nodeLevel *nextLevel;  //apuntador tipo struct que apunta a siguiente nivel
     struct nodeLevel *nextStage;  //apuntador tipo struct que apunta a siguiente escenario
+    int numLevel;
 }*NODE_LEVEL;
 
 /*Estructuras de tipo Escenario, son representados como vertices de un grafo enlazado*/
 typedef struct nodeStage
 {
-    struct nodeStage *nextStage;  //apuntador de tipo struct que apunta al siguiente escenario
+    struct nodeStage *nextStage;  //apuntador de tipo struct que apunta al siguiente
+    //escenario
     LIST_SPRITE listSprites;  //Lista dinamica de sprites
     NODE_LEVEL headLevel;  //cabecera de tipo NODE_LEVEL anidada dentro de el grafo
+    char nameStage[SIZE_STRINGS];
 }*NODE_STAGE;
 
 void initStages(NODE_STAGE *stageAct);
@@ -77,27 +83,99 @@ int main()
 
 }
 
-void initStages(NODE_STAGE *actualStage)
+void initStages(NODE_STAGE *actualStage)  /*Esta funcion, se encaraga de inicalizar los escenarios, poniendo su apuntador a NULL*/
 {
     *actualStage = NULL;
 }
 
-int createNodeStage(NODE_STAGE *newStage)
+int createNodeStage(NODE_STAGE *newStage, char name[])  /*Esta funcion, crea un nodo nuevo para el escenario donde se van a cargar los datos*/
 {
     int res = 0;
-    *newStage = (NODE_STAGE)malloc(sizeof(struct nodeStage));
+    *newStage = (NODE_STAGE)malloc(sizeof(struct nodeStage));  //obtiene memoria para el nuevo escenario
 
     if (newStage)
     {
-        (*newStage)->nextStage = NULL;
-        (*newStage)->listSprites = NULL;
-        (*newStage)->headLevel = NULL;
+        (*newStage)->nextStage = NULL;  //siguiente escenario a NULL, hasta que se cargue
+        (*newStage)->listSprites = NULL;  //cabecera de lista de sprites, a NULL, hasta que se carguen
+        (*newStage)->headLevel = NULL;  //cabecera de lista de niveles a NULL, hasta ser cargados
+        strcpy((*newStage)->nameStage, name);  //asignacion del nombre del stage, sin la extension
         res = 1;
     }
     return (res);
 }
 
+int createNodeLevel(NODE_LEVEL *newLevel)
+{
+    int res = 0;
+    *newLevel = (NODE_LEVEL)malloc(sizeof(struct nodeLevel));
 
+    if (newLevel)
+    {
+        (*newLevel)->nextLevel = NULL;
+        (*newLevel)->nextStage = NULL;
+        res = 1;
+    }
+    return(res);
+}
+
+//****************************************************************
+
+int createNodeSprite(LIST_SPRITE *newSprite, int x, int y, int visibility)  //funcion pendiente
+{
+    int res = 0;
+    *newSprite = (LIST_SPRITE)malloc(sizeof(struct nodeSprite));
+
+    if (newSprite)
+    {
+        (*newSprite)->fowSprite = NULL;
+        (*newSprite)->nextSprite = NULL;
+        (*newSprite)->sCorx = x;
+        (*newSprite)->sCory = y;
+        (*newSprite)->sVisible = visibility;
+        res = 1;
+    }
+    return (res);
+}
+
+//**************************************************************
+
+int searchArchive(char nameFile[], char message[])
+{
+    int res;
+    FILE *file;
+    *file = fopen(nameFile,"rb");
+
+    if (file)
+    {
+        res = 1;
+        strcpy(message, "Cargando...")
+        fclose(file);
+    }
+    else
+    {
+        res = 0;
+        strcpy(message, "Error 001: EL archivo no se encuentra o ha cambiado su nombre o extension");
+    }
+    return (res);
+}
+
+void loadStageArchive(char nameStageFile[])
+{
+    int res;
+    char messageError[40];
+    FILE *fileStage;
+    res = searchArchive(nameStageFile, messageError);
+
+    if (res)
+    {
+        *fileStage = fopen(nameStageFile, "rb");
+
+        if (fileStage)
+        {
+
+        }
+    }
+}
 
 
 
